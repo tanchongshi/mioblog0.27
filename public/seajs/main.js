ae69d6b35540c4cdfa0d0f8ce611fc24a1abc4c5
Chongshi Tam <339136347@qq.com>
2014-10-02 01:13:00 +0800

    seajs

---
+++ public/seajs/main.js
@@ -0,0 +1,5 @@
+define(function(require, exports, module) {   
+    var css = require('/miofront/style.css'); //引入css文件
+	var css = require('/miofront/css/font-awesome.min.css');
+	var css = require('/miofront/css/miolayout.css');
+});
